66e2ce0b0baf53039ab06cda334679151fc849ab
OleksandrProtchenko <alexdevelop92@gmail.com>
2026-02-14 20:55:40 +0200

    commit 2

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api';
-const API_KEY = import.meta.env.VITE_API_KEY;
-console.log(API_KEY);
+// const API_KEY = import.meta.env.VITE_API_KEY;
+// console.log(API_KEY);
 
 axios.defaults.baseURL = BASE_URL;
 
 function getImagesByQuery(query) {
   return axios({
-    url: `${BASE_URL}/?key=${API_KEY}`,
+    url: `${BASE_URL}/?key=${VITE_API_KEY}`,
     method: 'get',
     params: {
-      apiKey: API_KEY,
+      apiKey: VITE_API_KEY,
       q: query,
       image_type: 'photo',
       orientation: 'horizontal',
